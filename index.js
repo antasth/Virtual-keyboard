82a095f6dc2d24d4cbb098171e9dc75507f969c2
antasth <antasth@mail.ru>
2023-04-26 17:36:25 +0300

    feat: add basic styles for textarea and keyboard

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 import buttons from './data/buttons.js';
 
-console.log(buttons);
-
 const body = document.querySelector('body');
 console.log(body);
 
+const content = document.createElement('div');
+content.classList.add('content');
 const keyboard = document.createElement('div');
-keyboard.classList.add('content');
+keyboard.classList.add('keyboard');
+const textArea = document.createElement('textarea');
+textArea.classList.add('text-area');
 
-body.append(keyboard);
+body.append(content);
+content.append(textArea, keyboard);
 
 const firstRow = buttons.filter((item) => item.row === 1);
 
